fd92dfa8f51c1a083a70364cbd8425c68641315d
Andre Miguel Charneca Duarte <andre.duarte222@gmail.com>
2022-11-17 17:01:23 +0100

    added energy model

---
+++ lib/EnergyCAModel.py
@@ -0,0 +1,93 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from lib.utils import debug
+
+class EnergyCAModel(nn.Module):
+    def __init__(self, channel_n, device, hidden_size=128):
+        super().__init__()
+
+        self.device = device
+        self.channel_n = channel_n
+
+        self.fc0 = nn.Linear(channel_n*3, hidden_size)
+        self.fc1 = nn.Linear(hidden_size, channel_n, bias=False)
+
+        # receives the perception vector and outputs mapping of fireRates
+        #self.fireRate_layer = nn.Linear(hidden_size, 1)# do this if input=hidden state
+        self.fireRate_layer = nn.Linear(channel_n*3, 1, bias=False) #else the input is the perception vector
+        with torch.no_grad():
+            self.fc1.weight.zero_()
+            self.fireRate_layer.weight.zero_()
+
+        self.to(self.device)
+
+    def alive(self, x):        
+        ''' If all cells in neighbourhood have alpha<0.1, then
+        cell is dead: all channels=0 '''
+        return F.max_pool2d(x[:, 3:4, :, :], kernel_size=3, stride=1, padding=1) > 0.1
+
+    def perceive(self, x, angle):
+
+        def _perceive_with(x, weight):
+            conv_weights = torch.from_numpy(weight.astype(np.float32)).to(self.device)
+            conv_weights = conv_weights.view(1,1,3,3).repeat(self.channel_n, 1, 1, 1)
+            return F.conv2d(x, conv_weights, padding=1, groups=self.channel_n)
+
+        # Gradients in perpendicular directions
+        dx = np.outer([1, 2, 1], [-1, 0, 1]) / 8.0  # Sobel filter
+        dy = dx.T
+        c = np.cos(angle*np.pi/180)
+        s = np.sin(angle*np.pi/180)
+        w1 = c*dx-s*dy
+        w2 = s*dx+c*dy
+
+        # concatenate both gradient directions and cell state
+        y1 = _perceive_with(x, w1)
+        y2 = _perceive_with(x, w2)
+        y = torch.cat((x,y1,y2),1)
+        return y
+
+    def update(self, x, angle):
+        x = x.transpose(1,3)
+        pre_life_mask = self.alive(x)
+
+        # pass through NN
+        dx = self.perceive(x, angle)
+        dx = dx.transpose(1,3)
+        # get fireRates with perception vector, fireRates of dead cells is = 0
+        fireRates = torch.sigmoid(self.fireRate_layer(dx)) * pre_life_mask.transpose(1,3)
+
+        dx = self.fc0(dx)
+        dx = F.relu(dx)
+
+        # get fireRates with hidden state
+        #fireRates = torch.sigmoid(self.fireRate_layer(dx))
+
+        #debug("fireRates.min()", "fireRates.max()", "dx.max()", "dx.min()")
+        dx = self.fc1(dx)
+
+        # stochastic cell updates
+        update_grid = torch.bernoulli(fireRates).float().to(self.device)
+        dx = dx * update_grid
+
+        x = x+dx.transpose(1,3)
+
+        # kill cells
+        post_life_mask = self.alive(x)
+        life_mask = (pre_life_mask & post_life_mask).float()
+        x = x * life_mask
+
+        return x.transpose(1,3), fireRates
+
+    def forward(self, x, steps=1, fire_rate=None, angle=0.0):
+        x_steps = []
+        fireRates_steps = []
+        for step in range(steps):
+            x, fireRates = self.update(x, angle)
+            x_steps.append(x)
+            fireRates_steps.append(fireRates)
+
+        return torch.stack(x_steps), torch.stack(fireRates_steps)
--- lib/utils.py
+++ lib/utils.py
@@ -2,6 +2,11 @@
 from __future__ import print_function
 import sys
 import numpy as np
+import imageio
+from lib.utils_vis import to_rgb
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation, ArtistAnimation
+from IPython.display import HTML
 
 def tup_distance(node1, node2, mode="Euclidean"):
     """
@@ -14,6 +19,7 @@ def tup_distance(node1, node2, mode="Euclidean"):
     else:
         raise ValueError("Unrecognized distance mode: "+mode)
 
+
 def mat_distance(mat1, mat2, mode="Euclidean"):
     """
     mode: "Manhattan", "Euclidean"
@@ -24,6 +30,7 @@ def mat_distance(mat1, mat2, mode="Euclidean"):
         return np.sum(np.abs(mat1-mat2), axis=-1)
     else:
         raise ValueError("Unrecognized distance mode: "+mode)
+
 
 def debug(*args, same_line=False):
     ''' Pass as strings, prints expression name and value '''
@@ -36,3 +43,193 @@ def debug(*args, same_line=False):
         print(var, '=', repr(eval(var, frame.f_globals, frame.f_locals)))
     if same_line: print('\n')
     
+
+def load_emoji(index, path="data/emoji.png"):
+    im = imageio.imread(path)
+    emoji = np.array(im[:, index*40:(index+1)*40].astype(np.float32))
+    emoji /= 255.0
+    return emoji
+
+def visualize_batch_ponder(x0, x, n_steps):
+  '''
+    View batch initial seed, state at halting and final state
+  '''
+  vis0 = to_rgb(x0)
+  vis_final = to_rgb(x[-1])
+  print('batch (before/after):')
+  plt.figure(figsize=[15,5])
+  for i in range(x0.shape[0]):
+      plt.subplot(3,x0.shape[0],i+1)
+      plt.imshow(vis0[i])
+      plt.axis('off')
+  for i in range(x0.shape[0]):
+      vis_halt = to_rgb(x[n_steps[i]-1, i, ...])
+      plt.subplot(3,x0.shape[0],i+1+x0.shape[0])
+      plt.imshow(vis_halt)
+      plt.text(0,0, "n_halt="+str(n_steps[i]))
+      plt.axis('off')
+  for i in range(x0.shape[0]):
+      plt.subplot(3,x0.shape[0],i+1+2*x0.shape[0])
+      plt.imshow(vis_final[i])
+      plt.text(0,0, "n_final="+str(x.shape[0]))
+      plt.axis('off')
+
+  plt.show()
+
+
+def visualize_batch_adapt(x0, x, lambdas_steps, update_grid_steps=None, progress_steps = 8):
+    '''
+      View batch initial seed, batch final state, and progression of lambdas
+      and update grid steps of the 1st batch item
+    '''
+    vis_final = to_rgb(x[-1])
+    vis_batch0 = to_rgb(x[:,0])
+    max_steps = x.shape[0]
+    print('batch (before/after):')
+    plt.figure(figsize=[25,12])
+    n_cols = max(x0.shape[0], progress_steps)
+    n_rows = 5
+
+    # final states
+    for i in range(vis_final.shape[0]):
+      plt.subplot(n_rows,n_cols,i+1)
+      plt.imshow(vis_final[i])
+      plt.text(0,0, "n_final="+str(max_steps))
+      plt.axis('off')
+
+    # visualize evolution of first in batch
+    for i in range(progress_steps):
+      plt.subplot(n_rows, n_cols, i+1+n_cols)
+      plt.imshow(vis_batch0[max_steps//progress_steps * (i+1)-1])
+      plt.text(0,0, "step="+str(max_steps//progress_steps * (i+1)-1))
+      plt.axis('off')
+
+    # visualize lambdas
+    for i in range(progress_steps):
+      plt.subplot(n_rows, n_cols, i+1+2*n_cols)
+      plt.imshow(lambdas_steps[max_steps//progress_steps * (i+1)-1,0])
+      plt.text(0,0, "lambdas at step="+str(max_steps//progress_steps * (i+1)-1))
+      plt.axis('off')
+    plt.colorbar()
+
+    # visualize update_grid
+    for i in range(progress_steps):
+        plt.subplot(n_rows, n_cols, i+1+3*n_cols)
+        plt.imshow(update_grid_steps[max_steps//progress_steps * (i+1)-1,0])
+        plt.text(0,0, "updates at step="+str(max_steps//progress_steps * (i+1)-1))
+        plt.axis('off')
+    plt.show()
+
+
+def visualize_batch_energy(x0, x, fireRates_steps, update_grid_steps=None, progress_steps = 8):
+    '''
+      View batch initial seed, batch final state, and progression of fireRates
+    of the 1st batch item
+    '''
+    vis_final = to_rgb(x[-1])
+    vis_batch0 = to_rgb(x[:,0])
+    max_steps = x.shape[0]
+    print('batch (before/after):')
+    plt.figure(figsize=[25,12])
+    n_cols = max(x0.shape[0], progress_steps)
+    n_rows = 5
+
+    # final states
+    for i in range(vis_final.shape[0]):
+      plt.subplot(n_rows,n_cols,i+1)
+      plt.imshow(vis_final[i])
+      plt.text(0,0, "n_final="+str(max_steps))
+      plt.axis('off')
+
+    # visualize evolution of first in batch
+    for i in range(progress_steps):
+      plt.subplot(n_rows, n_cols, i+1+n_cols)
+      plt.imshow(vis_batch0[max_steps//progress_steps * (i+1)-1])
+      plt.text(0,0, "step="+str(max_steps//progress_steps * (i+1)-1))
+      plt.axis('off')
+
+    # visualize fireRates
+    for i in range(progress_steps):
+      plt.subplot(n_rows, n_cols, i+1+2*n_cols)
+      plt.imshow(fireRates_steps[max_steps//progress_steps * (i+1)-1,0])
+      plt.text(0,0, "fireRates step="+str(max_steps//progress_steps * (i+1)-1))
+      plt.axis('off')
+      plt.colorbar()
+    plt.show()
+
+
+def visualize_batch(x0, x):
+    vis0 = to_rgb(x0)
+    vis_final = to_rgb(x)
+    print('batch (before/after):')
+    plt.figure(figsize=[15,5])
+    for i in range(x0.shape[0]):
+        plt.subplot(3,x0.shape[0],i+1)
+        plt.imshow(vis0[i])
+        plt.axis('off')
+    for i in range(x0.shape[0]):
+        plt.subplot(3,x0.shape[0],i+1+x0.shape[0])
+        plt.imshow(vis_final[i])
+        plt.axis('off')
+
+    plt.show()
+
+def plot_loss_reg_rec(loss_log, loss_reg_log, loss_rec_log):
+    fig, (ax1, ax2, ax3) = plt.subplots(figsize=[25,5],nrows=1, ncols=3, sharey=True)
+  
+    ax1.set_title('total loss (log10)')
+    ax1.plot(np.log10(loss_log), '.', alpha=0.2, label='total')
+    
+    ax2.set_title('beta*regularization loss (log10)')
+    ax2.plot(np.log10(loss_reg_log), 'g.', alpha=0.2, label='beta*regularization')
+
+    ax3.set_title('reconstruction loss (log10)')
+    ax3.plot(np.log10(loss_rec_log), 'r.', alpha=0.2, label='reconstruction')
+    plt.show()
+
+def plot_loss(loss_log):
+    plt.figure(figsize=(10, 4))
+    plt.title('Loss history (log10)')
+    plt.plot(np.log10(loss_log), '.', alpha=0.2, label='total')
+    plt.legend()
+    plt.show()
+
+def plot_avg_steps(avg_steps_log):
+    plt.figure(figsize=(10, 4))
+    plt.title('Avg steps history')
+    plt.plot(avg_steps_log, 'g.', alpha=0.1)
+    plt.show()
+
+def animate_steps(*arr_steps, colorbar_plots=None, interval=40):
+  ''' 
+  Input: arrays to animate. List of indexes of the plots that need colorbars.
+  arr_steps[i].shape = (max_steps, batch_size, img_size, img_size) 
+  colorbar_plots = list of indexes of the plots with colorbars (assuming values go between 0 and 1)
+  '''
+  max_steps = arr_steps[0].shape[0]
+  num_plots = len(arr_steps)
+  fig, axs = plt.subplots(1, num_plots)
+  frames = []
+  if num_plots==1: axs = [axs]
+
+  for i in range(num_plots):
+    axs[i].imshow(arr_steps[i][0,0])
+    axs[i].get_xaxis().set_visible(False)
+    axs[i].get_yaxis().set_visible(False)
+
+  for k in range(max_steps):
+    ims = []
+    ttl = plt.text(0.5, 1.01, f"Step {k}", horizontalalignment='center', verticalalignment='bottom', transform=axs[0].transAxes)
+    for i in range(num_plots):
+      ims.append(axs[i].imshow(arr_steps[i][k,0], animated=True, vmin=0, vmax=1))
+    ims.append(ttl)  
+    frames.append(ims)
+
+  # colorbars
+  if colorbar_plots is not None:
+    for i in colorbar_plots:
+      fig.colorbar(ims[i], ax=axs[i],fraction=0.046, pad=0.04)
+  
+  anim = ArtistAnimation(fig, frames, interval=interval, blit=True)
+  plt.close()
+  return HTML(anim.to_html5_video())
